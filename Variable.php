a4f46571ba33e271f6de50a8fe88c35d4e5ef90e
원치현 <heungeob1003@gmail.com>
2021-09-30 17:59:19 +0900

    Variable.php 변수

---
+++ Variable.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Variable</h1>
+        <?php
+            $a = 1;
+            $b = 10;
+            echo $a + 1;
+            echo $b + 1;
+        ?>
+    </body>
+</html>
