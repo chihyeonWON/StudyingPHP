fcd43817bdd4f5dd1797afd9f34770acd6f04c07
원치현 <heungeob1003@gmail.com>
2021-09-30 17:50:18 +0900

    String.php 문자열

---
+++ String.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>String</h1>
+        <?php
+            echo "Hello World";
+            echo 'Hello "w"orld';
+            echo "Hello \"w\"orld";  // 역슬래쉬를 이용하면 그 역할에서 해제되고 순수한 문자로 사용된다
+        ?>
+    </body>
+</html>
