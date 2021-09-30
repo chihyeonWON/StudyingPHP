5f76ae38b0a3997af2c40b90fc1731ead448fd61
원치현 <heungeob1003@gmail.com>
2021-09-30 17:55:39 +0900

    strlen function 문자열의개수반환함수

---
+++ strlen.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>strlen함수</h1>
+        <?php
+            echo strlen("Hello World"); // 문자열을 strlen함수로 감싸면 문자열이 몇 개의 문자로 이뤄져 있는지 알려준다
+        ?>
+    </body>
+</html>
