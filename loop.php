b999fd4678021a5a86a2d611dbaccc045b60797a
원치현 <heungeob1003@gmail.com>
2021-10-02 21:18:25 +0900

    Add loop.php

---
+++ loop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+          echo "1<br>";
+          $i = 0;
+          while($i < 3){
+              echo "2<br>";
+              $i = $i + 1;
+          }  
+          echo "3<br>";
+        ?>
+    </body>
+</html>
