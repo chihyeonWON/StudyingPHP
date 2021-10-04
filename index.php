5bf822d606c03810ccb77a8b136bd822bd2ddb79
원치현 <heungeob1003@gmail.com>
2021-10-04 15:47:11 +0900

    Paste h2 code to title that means title

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>
+            <?php
+                if(isset($_GET["id"])) {
+                    echo $_GET["id"];
+                }
+                else {
+                    echo "Welcome";
+                }
+            ?>
+        </title>
     </head>
     <body>
         <h1><a href="index.php">WEB</a></h1>
