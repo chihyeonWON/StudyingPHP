88a43fb4893fe85566915e6dc1b2e39fb91dc74b
원치현 <heungeob1003@gmail.com>
2021-10-04 15:53:18 +0900

    Build print list funtion and apply this function to ol's firstchild tag

--- index.php
+++ index.php
@@ -15,6 +15,18 @@
                 echo "Hello, PHP";
             }
     }
+    function print_list() {
+        $list = scandir("./data");
+                $i = 0;
+                while($i < count($list)) {
+                    if($list[$i] != "."){
+                        if($list[$i] != ".."){
+                            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";        
+                        }
+                    }
+                    $i += 1;
+                }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -31,16 +43,7 @@
         <h1><a href="index.php">WEB</a></h1>
         <ol>
             <?php
-                $list = scandir("./data");
-                $i = 0;
-                while($i < count($list)) {
-                    if($list[$i] != "."){
-                        if($list[$i] != ".."){
-                            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";        
-                        }
-                    }
-                    $i += 1;
-                }
+                print_list();
                 /*
                 echo "<li>$list[0]</li>\n";
                 echo "<li>$list[1]</li>\n";
