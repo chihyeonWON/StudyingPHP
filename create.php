c19fd7f372f6fd0bd8e059a609b39238369f181c
원치현 <heungeob1003@gmail.com>
2021-10-06 19:12:04 +0900

    Require top.php

--- create.php
+++ create.php
@@ -1,31 +1,9 @@
 <?php
     require("lib/print.php");
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>
-            <?php
-                print_title();
-            ?>
-        </title>
-    </head>
-    <body>
-        <h1><a href="index.php">WEB</a></h1>
-        <ol>
-            <?php
-                print_list();
-                /*
-                echo "<li>$list[0]</li>\n";
-                echo "<li>$list[1]</li>\n";
-                echo "<li>$list[2]</li>\n";
-                echo "<li>$list[3]</li>\n";
-                echo "<li>$list[4]</li>\n";
-                echo "<li>$list[5]</li>\n";
-                */
-            ?>
-        </ol>
+<?php
+    require("view/top.php");
+?>
         <a href="create.php">create</a>
         <form action="create_process.php" method="post">
             <p>
