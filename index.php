af0af3cdee0570f3502fdfc28c426213fce24dde
원치현 <heungeob1003@gmail.com>
2021-10-06 19:00:31 +0900

    Edit delete link code to form's post method

--- index.php
+++ index.php
@@ -57,7 +57,10 @@
         <a href="create.php">create</a>
         <?php if(isset($_GET["id"])){ ?>
                 <a href="update.php?id=<?=$_GET["id"]?>">update</a>
-                <a href="delete_process.php?id=<?=$_GET["id"]?>">delete</a>
+                <form action="delete_process.php" method="post">
+                    <input type="hidden" name="id" value="<?=$_GET["id"]?>" />
+                    <input type="submit" value="delete" /> 
+                </form>
         <?php } ?>  
           <h2>
             <?php
