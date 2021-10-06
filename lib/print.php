f3bb8ae1d6a6f76773a4a82800ed49a5412db0cc
원치현 <heungeob1003@gmail.com>
2021-10-06 19:17:57 +0900

    Use htmlspeciachars function to prevent alternation

--- lib/print.php
+++ lib/print.php
@@ -1,7 +1,7 @@
 <?php
     function print_title() {
         if(isset($_GET["id"])) {
-                    echo $_GET["id"];
+                    echo htmlspecialchars($_GET["id"]);
                 }
                 else {
                     echo "Welcome";
@@ -9,7 +9,7 @@
     }
     function print_description(){
         if(isset($_GET["id"])) {
-                echo file_get_contents("data/".$_GET["id"]);
+                echo htmlspecialchars(file_get_contents("data/".$_GET["id"]));
             }
             else {
                 echo "Hello, PHP";
@@ -19,9 +19,10 @@
         $list = scandir("./data");
                 $i = 0;
                 while($i < count($list)) {
+                    $title = htmlspecialchars($list[$i]);
                     if($list[$i] != "."){
                         if($list[$i] != ".."){
-                            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";        
+                            echo "<li><a href=\"index.php?id=$title\">$title</a></li>\n";        
                         }
                     }
                     $i += 1;
